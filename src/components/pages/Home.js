c6c4a37a8f4c0e3e92667eabd07bc06a9f53fc80
Fernando Giraudo <fergiraudo91@gmail.com>
2021-04-01 11:58:55 -0300

    counter

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Button } from "@material-ui/core";
+import React, { useState } from "react";
+import './Home.css';
 
 export const Home = () => {
-    return (
-        <>
-           <h1 className="home">Opressus</h1> 
-        </>
-    )
-}
+    const [count, setCount] = useState(0);
+  return (
+    <>
+        <div className="btn-container">
+      <Button variant="contained" color="primary" onClick={() => setCount(count + 1)}>
+        +
+      </Button>
+      <Button variant="contained" color="secondary" onClick={() => count > 0 && setCount(count - 1)}>
+        -
+      </Button>
+      </div>
+      <h1 className="result">{count}</h1>
+      
+    </>
+  );
+};
