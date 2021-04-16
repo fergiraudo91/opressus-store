8804500565dc90641fe571275dc7dd4b605bcba6
Fernando Giraudo <fergiraudo91@gmail.com>
2021-04-16 00:22:20 -0300

    some modifications

--- src/components/Items/ListItems/ItemsList.js
+++ src/components/Items/ListItems/ItemsList.js
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import "./ItemList.css";
 import { Item } from "../SingleItem/Item";
+import { useParams } from "react-router";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -18,8 +19,10 @@ const useStyles = makeStyles((theme) => ({
 
 export const ItemsList = () => {
   const [data, setData] = useState([]);
-
+  const { type } = useParams();
   useEffect(() => {
+    let filter;
+
     fetch("data/beers.json", {
       headers: {
         "Content-Type": "application/json",
@@ -27,15 +30,47 @@ export const ItemsList = () => {
       },
     })
       .then((response) => response.json())
-      .then((data) => setData(data));
-  }, []);
+      .then((data) => {
+        switch (type) {
+          case "beer":
+            filter = data.filter(
+              (item) =>
+                item.type === "red" ||
+                item.type === "golden" ||
+                item.type === "black" ||
+                item.type === "IPA"
+            );
+            break;
+          case "wines":
+            filter = data.filter((item) => item.type === "wine");
+            break;
+          case "ipa":
+            filter = data.filter((item) => item.type === "IPA");
+            break;
+          case "golden":
+            filter = data.filter((item) => item.type === "golden");
+            break;
+          case "red":
+            filter = data.filter((item) => item.type === "red");
+            break;
+            case "black":
+              filter = data.filter((item) => item.type === "black");
+              break;
+
+          default:
+            filter = data;
+            break;
+        }
+        setData(filter);
+      });
+  }, [type]);
 
   const classes = useStyles();
   return (
     <div className={classes.root + " items-container"}>
       <Grid container spacing={4} justify="center" alignItems="center">
         <Grid item xs={12}>
-          <h1 className="title">List of Drinks</h1>
+          <h1 className="title">List of {!!type ? type : 'Drinks'}</h1>
         </Grid>
         {data.map((beer) => {
           return (
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,31 +5,29 @@ export const Contact = () => {
     <div className="container mt-5">
       <h1 className="text-center">Contact</h1>
       <form>
-        <div class="form-group">
-          <label for="exampleInputEmail1">Email address</label>
+        <div className="form-group">
+          <label htmlFor="text">Name</label>
+          <input type="text" className="form-control" id="name" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email address</label>
           <input
             type="email"
-            class="form-control"
+            className="form-control"
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
           />
-          <small id="emailHelp" class="form-text text-muted">
+          <small id="emailHelp" className="form-text text-muted">
             We'll never share your email with anyone else.
           </small>
         </div>
-        <div class="form-group">
-          <label for="exampleInputPassword1">Password</label>
-          <input
-            type="password"
-            class="form-control"
-            id="exampleInputPassword1"
-          />
-        </div>
-        <div class="form-group form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-          <label class="form-check-label" for="exampleCheck1">
-            Check me out
-          </label>
+        <div className="form-group">
+          <label htmlFor="message">Message</label>
+          <textarea
+            className="form-control"
+            id="message"
+            rows="3"
+          ></textarea>
         </div>
         <Button variant="contained" color="primary" type="submit">
           Submit
