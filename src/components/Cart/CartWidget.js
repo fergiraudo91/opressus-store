5de2065b4c2b23c80994c614fc62feffcda5757e
Fernando Giraudo <fergiraudo91@gmail.com>
2021-04-22 20:34:43 -0300

    cambios en el cart icon

--- src/components/Cart/CartWidget.js
+++ src/components/Cart/CartWidget.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import { CartContext } from "../context/CartContext";
 
 export const CartWidget = (props) => {
+  const { cart } = useContext(CartContext);
+  const [quantity, setQuantity] = useState(0);
+  useEffect(() => {
+    if (cart.length === 1) {
+      if (!!cart[0].prices) {
+        setQuantity(cart[0].quantity);
+      }
+    } else {
+      setQuantity(cart.reduce((prev, next) => prev.quantity + next.quantity));
+    }
+  }, [cart, quantity])
+
+
+  
+
   return (
-    <li className="nav-item">
-      <Link to="/cart" className="nav-links" onClick={props.onClick}>
-        Cart <ShoppingCartIcon />
-      </Link>
-    </li>
+    quantity > 0 && (
+      <li className="nav-item">
+        <Link to="/cart" className="nav-links" onClick={props.onClick}>
+          <ShoppingCartIcon /> {quantity}
+        </Link>
+      </li>
+    )
   );
 };
