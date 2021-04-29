e8c5fcd0497117c215a1ba3cb68fdbe9f1ab46d9
fergiraudo91 <fergiraudo91@gmail.com>
2021-04-28 22:23:29 -0300

    fix button

--- src/components/Items/SingleItem/Item.js
+++ src/components/Items/SingleItem/Item.js
@@ -10,41 +10,43 @@ import Typography from '@material-ui/core/Typography';
 import './Item.css';
 import { Link } from 'react-router-dom';
 const useStyles = makeStyles({
-    root: {
-      maxWidth: 345,
-    },
-  });
+  root: {
+    maxWidth: 345,
+  },
+});
 
 
 
-export const Item = ({imageUrl, title, description, prices, id}) => {
-    const classes = useStyles();
+export const Item = ({ imageUrl, title, description, prices, id }) => {
+  const classes = useStyles();
 
   return (
     <Card className={classes.root}>
       <Link to={`../drink/${id}`}>
-      <CardActionArea >
-        <CardMedia
-          component="img"
-          alt="beer"
-          height="300"
-          image={imageUrl}
-          title="Beer House"
-        />
-        <CardContent className="card-content">
-          <Typography gutterBottom variant="subtitle1" component="h3" align="center">
-          {title}
-          </Typography>
-          {/* <Typography variant="body2" color="textSecondary" component="p">
+        <CardActionArea >
+          <CardMedia
+            component="img"
+            alt="beer"
+            height="300"
+            image={imageUrl}
+            title="Beer House"
+          />
+          <CardContent className="card-content">
+            <Typography gutterBottom variant="subtitle1" component="h3" align="center">
+              {title}
+            </Typography>
+            {/* <Typography variant="body2" color="textSecondary" component="p">
             {description}
           </Typography> */}
-        </CardContent>
-      </CardActionArea>
+          </CardContent>
+        </CardActionArea>
       </Link>
       <CardActions>
-        <Button size="large" variant="contained" color="primary" className="buy-btn" fullWidth>
-          Buy
+        <Link to={`../drink/${id}`} className="w-100">
+          <Button size="large" variant="contained" color="primary" className="buy-btn" fullWidth>
+            Ver Producto
         </Button>
+        </Link>
       </CardActions>
     </Card>
   );
